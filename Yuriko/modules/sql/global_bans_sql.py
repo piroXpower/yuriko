294d94eb9d7598c3b2c46d3acbb6507f6ce0f06c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2021-12-25 18:39:40 +0530

    Update global_bans_sql.py

--- Yuriko/modules/sql/global_bans_sql.py
+++ Yuriko/modules/sql/global_bans_sql.py
@@ -1,12 +1,12 @@
 import threading
-from sqlalchemy.sql.sqltypes import BigInteger
+
 from Yuriko.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, String, UnicodeText
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
 class GloballyBannedUsers(BASE):
     __tablename__ = "gbans"
-    user_id = Column(BigInteger, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     name = Column(UnicodeText, nullable=False)
     reason = Column(UnicodeText)
 
